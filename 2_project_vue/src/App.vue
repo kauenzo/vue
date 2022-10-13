8523cfc4ef3d030d144476724a3e00ec6ee18bfe
kaue.elias <kaue.elias@engeplus.com.br>
2022-10-13 16:48:53 -0300

    dados em componentes (data)

--- 2_project_vue/src/App.vue
+++ 2_project_vue/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <primeiroComponente/>
+  <p>O dado de app é: {{testando}}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import primeiroComponente from "./components/primeiroComponente.vue";
+  export default {
+    name: 'App',
+    components: {
+      primeiroComponente
+    },
+    data(){
+      return{
+        testando: "teste"
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ 2_project_vue/src/components/primeiroComponente.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1> Ola Vue!</h1>
+  <p> meu nome é {{ nome }}, e sou {{profissao}}</p>
+</template>
+
+<script>
+export default {
+  name: "primeiroComponente",
+  data(){
+    return{
+      nome: "Kaue",
+      profissao: "Dev"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
