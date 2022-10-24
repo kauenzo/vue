f61c95256568bce5b03acb371b4c11703045cadf
kaue.elias <kaue.elias@engeplus.com.br>
2022-10-24 16:49:39 -0300

    $emit - ouvir eventos de componentes filho no componente pai

--- 2_project_vue/src/components/Info.vue
+++ 2_project_vue/src/components/Info.vue
@@ -1,4 +1,5 @@
 <template>
+  <Picture />
   <p> {{email}}</p>
   <span v-if="possuiOShape">O shape fala por mim. </span>
   <!--  <div>
@@ -6,21 +7,16 @@
       <span v-if="possuiOShape">O shape fala por mim. </span>
       <span v-else>Se você joga Minecraft, obrigatóriamente precisa estar no shape. </span>
       <span v-show="isMadrugada">De {{ horario }} minha mente vai longe. </span>
-      <treino></treino>
       <button @click="showShape" class="botao-shape">{{ texto_botao }}</button>
-      <p v-show="treino">Às vezes não sinto vontade de treinar, mas sei dos benefícios de estar no shape.</p>
       <p>Entrar no <a v-bind:href="link_shape">shape</a></p>
-      <Picture></Picture>
     </div>-->
 </template>
 
 <script>
 import Picture from "@/components/Picture";
-import Treino from "@/components/Info/treino";
 export default {
   name: "Info",
   components: {
-    Treino,
     Picture
   },
   props: {
@@ -52,6 +48,6 @@ export default {
 <style scoped>
   div{
     /*border: 1px solid red;*/
-    background-color: #999999;
+    /*background-color: #999999;*/
   }
 </style>
--- 2_project_vue/src/components/Picture.vue
+++ 2_project_vue/src/components/Picture.vue
@@ -1,14 +1,25 @@
 <template>
-  <img :src="avatar" :alt="descricao">
+  <div>
+    <MudarImagem @mudar-imagem="mudarImagem"/>
+    <img :src="avatar" :alt="descricao">
+  </div>
 </template>
 
 <script>
+import MudarImagem from "@/components/MudarImagem";
 export default {
   name: "Picture",
-  data(){
-    return{
+  components: {MudarImagem},
+  data() {
+    return {
       avatar: "/img/avatar-user.png",
       descricao: "Kaue Elias"
+    }
+  },
+  methods: {
+    mudarImagem() {
+      console.log('imagem trocada');
+      this.avatar = "/img/avatar2.png";
     }
   }
 }
