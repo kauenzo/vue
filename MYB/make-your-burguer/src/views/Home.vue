38b5e2d5bf895b892034655b51a1529bbe62f69a
kaue.elias <kaue.elias@engeplus.com.br>
2022-10-27 17:43:04 -0300

    banner

--- MYB/make-your-burguer/src/views/Home.vue
+++ MYB/make-your-burguer/src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu hamburguer</h1>
+    <p>formulário</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import Banner from "@/components/Banner";
 export default {
   name: 'Home',
   components: {
+    Banner
   }
 }
 </script>
