fc390dc279d8aa6edd5892d2f2b52e167bc15a7a
kaue.elias <kaue.elias@engeplus.com.br>
2022-10-27 17:20:25 -0300

    rodapé finalizado

---
+++ MYB/make-your-burguer/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main-container">
+    <h1>HOME</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
