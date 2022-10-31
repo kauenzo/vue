931986c134a9b0c59ff5b5f43ae713c15a18a0e5
kaue.elias <kaue.elias@engeplus.com.br>
2022-10-31 08:58:49 -0300

    criado tela de pedidos / n finalizado

--- MYB/make-your-burguer/src/components/Dashboard.vue
+++ MYB/make-your-burguer/src/components/Dashboard.vue
@@ -1,66 +1,81 @@
 <template>
   <div id="burger-table">
-    <table class="table-main">
-      <thead id="burger-table-heading">
-        <th class="order-id">#</th>
-        <th>Cliente: </th>
-        <th>Pão: </th>
-        <th>Carne: </th>
-        <th>Opcionais: </th>
-        <th>Ações: </th>
-      </thead>
-      <tbody id="burger-table-rows"  style="border: 1px solid red">
-        <tr class="burger-table-row">
-          <td class="order-number">1</td>
-          <td>joao</td>
-          <td>trigo</td>
-          <td>maminha</td>
-          <td>
+    <div class="table-main">
+      <div id="burger-table-heading">
+        <div class="order-id">#</div>
+        <div>Cliente: </div>
+        <div>Pão: </div>
+        <div>Carne: </div>
+        <div>Opcionais: </div>
+        <div>Ações: </div>
+      </div>
+      <div id="burger-table-rows">
+        <div class="burger-table-row" v-for="burger in burgers" :key="burger_id">
+          <div class="order-number">{{ burger.id }}</div>
+          <div>{{ burger.nome }}</div>
+          <div>{{ burger.pao }}</div>
+          <div>{{ burger.carne }}</div>
+          <div>
             <ul>
-              <li>Salame</li>
-              <li>Tomate</li>
+              <li v-for="(opcional, index) in burger.opcionais">
+                {{ opcional }}
+              </li>
             </ul>
-          </td>
-          <td>
+          </div>
+          <div>
             <select name="status" class="status">
               <option value="">Selecione o status</option>
             </select>
             <button class="delete-btn">Cancelar</button>
-          </td>
-        </tr>
-        <tr class="burger-table-row">
-          <td class="order-number" style="border: 1px solid green">2</td>
-          <td>joao</td>
-          <td>trigo</td>
-          <td>maminha</td>
-          <td>
-            <ul>
-              <li>Salame</li>
-              <li>Tomate</li>
-            </ul>
-          </td>
-          <td>
-            <select name="status" class="status">
-              <option value="">Selecione o status</option>
-            </select>
-            <button class="delete-btn">Cancelar</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  data(){
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    }
+  },
+  methods: {
+    async getPedidos(){
+      const req = await fetch("http://localhost:3000/burgers");
+
+      const data = await req.json();
+
+      this.burgers = data;
+
+      //resgatar os status
+
+      console.log(this.burgers)
+    }
+  },
+  mounted() {
+    this.getPedidos();
+  }
 }
 </script>
 
 <style scoped>
+div{
+  border: 0.5px solid red;
+}
+  .table-main div {
+    /*border: 1px solid red;*/
+    padding: 0 10px;
+  }
+
   #burger-table {
     max-width: 1200px;
     margin: 0 auto;
+    /*border: 1px solid blue;*/
   }
 
   #burger-table-heading,
